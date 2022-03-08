1d922bff955b36fa749ee02515cf56153236d648
CharaD7 <jijakahn6@gmail.com>
2022-03-08 00:55:10 +0000

    Completed navs for right div - Completed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
 			<div className="flex items-center space-x-5">
 				<Link href="/">
 					<img className="w-44 object-contain cursor-pointer" src="https://links.papareact.com/yvf" alt="" />
@@ -13,7 +13,10 @@ function Header() {
 					<h3 className="text-white bg-green-600 px-4 py-1 rounded-full">Follow</h3>
 				</div>
 			</div>
-			<div></div>
+			<div className="text-green-600 flex items-center space-x-5">
+			<h3>Sign In</h3>
+			<h3 className="border px-4 py-1 rounded-full border-b-green-600">Get Started</h3>
+			</div>
 		</header>
   )
 }
